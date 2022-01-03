f4433d16b02a1d28b189fa166143bcc23905bb62
nabin <nkbhaie2017@gmail.com>
2022-01-03 15:18:31 -0700

    modify step1.js file so that it is easy to import in step2.js file

--- step1.js
+++ step1.js
@@ -1,5 +1,7 @@
 const fs = require('fs')  // file system
 
+// step 1
+
 function cat(path){  // path is the location of the file
     // req.fileRead(filename.txt, encoding, callback){
     fs.readFile(path, 'utf8', (err, data) =>{
@@ -9,11 +11,19 @@ function cat(path){  // path is the location of the file
             // process.kill(1);
             process.exit(1);
         }
-        console.log(data);
+        console.log(data);  // no parsing of data is needed
+        // return data;
     })
 }
 
 // process.argv is an object that contains 2 things-location of node and location of the file in your computer
-cat(process.argv[2]);  // process.argv contains 2 things, first the location of the node and second the location of the local file
-
+let value = cat(process.argv[2]);  // process.argv contains 2 things, first the location of the node and second the location of the local file
+console.log(value);
 // in terminal do node step1.js one.txt
+
+
+/* To import this function in step2.js file */
+module.exports = {
+    cat: cat
+    // value: value
+}
